5e623d1d6e3edcd2463aa252c7933ce0ef2f4706
baketnk <baketnk@proton.me>
2025-08-23 18:46:48 -0400

    add top level add_model function for easier init

--- lua/lnvim/init.lua
+++ lua/lnvim/init.lua
@@ -1,6 +1,12 @@
 local M = {}
 -- module references
 local cfg = require("lnvim.cfg")
+local state = require("lnvim.state")
+
+-- local required_fields = { "model_id", "model_type", "api_url", "api_key" }
+function M.add_model(model)
+	table.insert(state.models, cfg.validate_model(model))
+end
 
 function M.setup(_opts)
 	cfg.setup(_opts)
